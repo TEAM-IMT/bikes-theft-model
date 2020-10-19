2f5934a188183e6c560a82c41d236f45de525585
TatianaMoreno47 <tatianamoreno47@gmail.com>
2020-10-19 03:02:36 +0200

    Graficas

---
+++ Codigos/DataStatistics.py
@@ -0,0 +1,82 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+'exec(%matplotlib inline)'
+import seaborn as sns
+import numpy as np
+import scipy as sp
+import plotly.express as px
+import plotly.graph_objects as go
+from statsmodels.graphics.mosaicplot import mosaic
+
+def readDB(filename):
+    return pd.read_csv(filename, header = 0)
+
+def Data_Shape(Data):
+    dataset_type=type(Data)
+    # the shape of data 
+    shape=Data.shape
+    #variables in data
+    col_names=Data.columns
+   
+    return dataset_type,shape,col_names
+
+def Data_Describe(Data):
+    num_des=Data.describe()
+    cate_des=Data.describe(include=['object'])
+
+    return num_des,cate_des
+
+def CountBar_plot(Data,name, title):
+    #Bar plot cout of name
+    fig = px.pie(Data, names=name, title=title)
+    fig.show()
+
+def Pie_plot(Data,name, title):
+    #Cicle % of name
+    fig = px.pie(Data, name, title)
+    fig.show()
+
+def Scatter_Plot(data,x_line,y_line): 
+    # Grahp x vs y
+    fig = px.scatter(data,x=x_line, y=y_line)
+    fig.show()
+
+def Scatter_Matrix(data,dim,color):
+    #Scater matrix of dim (list[]) by color
+    fig = px.scatter_matrix(data, dimensions=dim, color=color)
+    fig.show()
+
+
+def Box_plot(data,x_line,y_line,orient):
+    #Box plot
+    fig = px.box(data,x=x_line, y=y_line,orientation=orient)
+    fig.show()
+
+def Bar_Stacket(data,X,Y):
+    # Bar stacket color =Y, count X
+    ax1=pd.crosstab(data.X,data.Y).plot.barh(stacked=True)
+
+    return ax1
+
+def Histogram_plot(data,x_line,color):
+    #Histogram of X_line per color
+    fig = px.histogram(data, x=x_line, color=color, barmode='group')
+    fig.show()
+
+def plot_corr(data):
+  corr = data.corr()
+  # Generate a mask for the upper triangle
+  mask = np.zeros_like(corr, dtype=np.bool)
+  mask[np.triu_indices_from(mask, k=1)] = True
+
+  # Generate a custom diverging colormap
+  cmap = sns.diverging_palette(220, 10, as_cmap=True)
+  sns.heatmap(corr, cmap=cmap, mask = mask)
+
+def Mosaic_square(data,x,y):
+    ax=mosaic(data,[x,y])
+    return ax 
+
+
+
+
